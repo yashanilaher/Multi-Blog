86b0f263168db5330f6407f99fefa086fbcf40ec
yashanilaher <122201031@smail.iitpkd.ac.in>
2025-06-18 18:50:22 +0530

    updated AuthToken.js

--- Backend/jwt/AuthToken.js
+++ Backend/jwt/AuthToken.js
@@ -9,7 +9,7 @@ const createTokenSaveCookies=async(userId,res)=>{
     //so here tooken is a long string 
 
     res.cookie("jwt",token,{
-        httpOnly:false, //xss
+        httpOnly:true, //xss
         secure:true,
         samesite:"none" //csrf
     })
